8d99451d09fdf5590519dd89a5b57bd86131ee8e
Per Landberg <per.landberg@polago.se>
2015-09-15 16:09:10 +0200

    Added FileSystem implementation for ConfigGroups

---
+++ src/main/java/org/polago/deployconf/group/FileSystemConfigGroup.java
@@ -0,0 +1,88 @@
+/**
+* Copyright (c) 2015 Polago AB
+* All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package org.polago.deployconf.group;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
+/**
+ * ConfigGroup that uses the file system for storing.
+ */
+public class FileSystemConfigGroup implements ConfigGroup {
+
+    private final Path path;
+
+    private final Properties properties;
+
+    /**
+     * Public Constructor.
+     *
+     * @param name the config group name
+     * @param dir the repository where to store config groups
+     * @throws IOException indicating IO error
+     */
+    public FileSystemConfigGroup(String name, Path dir) throws IOException {
+        path = dir.resolve(name + "-config-group.xml");
+        properties = new Properties();
+
+        if (Files.exists(path)) {
+            InputStream is = Files.newInputStream(path);
+            try {
+                properties.loadFromXML(is);
+            } finally {
+                is.close();
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getProperty(String name) {
+        return properties.getProperty(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setProperty(String name, String value) throws IOException {
+        properties.setProperty(name, value);
+        OutputStream os = Files.newOutputStream(path, StandardOpenOption.CREATE);
+        try {
+            properties.storeToXML(os, null);
+        } finally {
+            os.close();
+        }
+
+    }
+
+}
